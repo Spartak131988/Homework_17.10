fe20cfda685e31453e37f0bc693f67a4c96c61d7
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-10-17 13:30:38 +0700

    Создали метод заполнения массива

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,1 +1,13 @@
 ﻿string[] arr = new string[20];
+
+void Fill(string[] array)
+{
+    Random res = new Random();
+    string str = "абвгдежзийклмнопрстуфхцчшщъыьэюя0123456789";
+    int size = array.Length;    
+    for (int i = 0; i < size; i++)
+    {
+        int index = res.Next(str.Length);        
+        array[i] = Convert.ToString(str[index]);
+    }    
+}
